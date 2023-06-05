60a24ebe6440aea0ff64651af0791ed559ede452
thomas maynard <thomas.maynard@etudiant.univ-lr.fr>
2023-06-05 11:32:14 +0200

    debut des cpp mais stopper par des fichiers pas encore existants

--- FastAsFox/FastAsFoxGame/level.cpp
+++ FastAsFox/FastAsFoxGame/level.cpp
@@ -1,6 +1,41 @@
 #include "level.h"
 
-Level::Level()
+Level::Level(pair<int,int> AStartingPosition, pair<int,int> AnEndingPosistion,Map AMap)
 {
+    startingPosition=AStartingPosition;
+    endingPosition=AnEndingPosistion;
+    map=AMap;
+}
 
+Level::~Level(){
+
+}
+
+
+void Level::loadMap(){
+
+}
+
+void Level::showMap(){
+
+}
+
+void Level::showScore(){
+
+}
+
+void Level::showUI(){
+
+}
+
+void Level::start(){
+
+}
+
+void Level::finish(){
+
+}
+
+Map Level::getMap(){
+    return map;
 }
