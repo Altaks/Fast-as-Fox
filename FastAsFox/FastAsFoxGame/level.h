a901ebf3a13015af8f618af28799cbf9fa4381c7
thomas maynard <thomas.maynard@etudiant.univ-lr.fr>
2023-06-05 10:59:24 +0200

    biggining class level

---
+++ FastAsFox/FastAsFoxGame/level.h
@@ -0,0 +1,34 @@
+#ifndef LEVEL_H
+#define LEVEL_H
+
+#include <QPair>
+#include <QElapsedTimer>
+
+
+using namespace std;
+
+#include "player.h"
+#include "gameobject.h"
+#include "map.h"
+
+class Level
+{
+private:
+    QElapsedTimer timescore;
+    pair<int,int> startingPosition;
+    GameObject * endingPosition;
+    Player * player;
+    Map map;
+    void loadMap();
+    void showMap();
+    void showScore();
+    void showUI();
+public:
+    Level(pair<int,int> startingPosition, pair<int,int> endingPosistion,Map map);
+    ~Level();
+    void start();
+    void finish();
+    Map getMap();
+};
+
+#endif // LEVEL_H
