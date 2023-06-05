8c3a4fe3458835906fb7a87f683465a94e7dbdb9
Denis <popescudenis317@gmail.com>
2023-06-05 16:14:09 +0200

    Revert "Merge branch 'dev' into dev-denis"
    This reverts commit a61819f75cd126053c76616729ac2afbe0dfe1a7, reversing
    changes made to a6f3bcbc0fe0a37de674930a739132356018c4fb.

--- FastAsFox/mapsectiontest/tst_mapsectiontest.cpp
+++ FastAsFox/mapsectiontest/tst_mapsectiontest.cpp
@@ -1,8 +1,4 @@
 #include <QtTest>
-#include <iostream>
-
-#include "../FastAsFoxGame/mapsection.h"
-#include "../FastAsFoxGame/constants.h"
 
 // add necessary includes here
 
@@ -10,46 +6,28 @@ class mapSectionTest : public QObject
 {
     Q_OBJECT
 
-    MapSection* mapSection;
-    std::pair<int, int> coordinates;
 public:
+    mapSectionTest();
+    ~mapSectionTest();
 
 private slots:
-    void initTestCase();
-    void init();
-
     void test_case1();
 
-    void cleanup();
-    void cleanupTestCase();
 };
 
-void mapSectionTest::initTestCase()
+mapSectionTest::mapSectionTest()
 {
-    mapSection = new MapSection(LEVEL_ONE);
+
 }
 
-void mapSectionTest::init()
+mapSectionTest::~mapSectionTest()
 {
 
 }
 
 void mapSectionTest::test_case1()
 {
-    for (std::map<std::pair<int, int>,int>::iterator it=mapSection->getCoordinatesToTileId().begin(); it!=mapSection->getCoordinatesToTileId().end(); it++)
-        std::cout << it->first.first << "; " << it->first.second << " => " << it->second << '\n';
-    //coordinates = std::pair<int,int>(0,0);
-    //QCOMPARE(mapSection->getCoordinatesToTileId().at(coordinates),0);
-}
 
-void mapSectionTest::cleanup()
-{
-
-}
-
-void mapSectionTest::cleanupTestCase()
-{
-    delete mapSection;
 }
 
 QTEST_APPLESS_MAIN(mapSectionTest)
