48bec70bf3d15bf41901d2f8138f08e0005939bd
Denis <popescudenis317@gmail.com>
2023-06-05 14:37:28 +0200

    debut game.h & game.cpp

--- FastAsFox/FastAsFoxGame/game.cpp
+++ FastAsFox/FastAsFoxGame/game.cpp
@@ -1,6 +1,45 @@
 #include "game.h"
 
-game::game()
+Game::Game(QObject *parent)
+    : QObject(parent)
 {
+    tps = new QTimer(this);
+    fps = new QTimer(this);
 
+    connect(tps, &QTimer::timeout, this, &Game::slotPositionUpdates);
+    tps->start(1000 / 20); // 20 Hz
+
+    connect(fps, &QTimer::timeout, this, &Game::slotRepaintingUpdates);
+    fps->start(1000 / 100); // 100 Hz
+}
+
+Game::~Game()
+{
+    delete tps;
+    delete fps;
+}
+
+void Game::slotPositionUpdates()
+{
+    // Perform position updates for game objects here
+
+    for (GameObject *gameObject : gameObjects) {
+        // Update the position of the game object
+        gameObject->updatePosition();
+    }
+
+    // Emit a signal or call a method to notify other parts of the program
+    // that the positions have been updated
+    emit positionsUpdated();
+}
+
+void Game::slotRepaintingUpdates()
+{
+    // Perform repainting or visual updates for the game here
+
+    // Repaint the game view or perform other visual updates
+    gameView->repaint();
+
+    // Emit a signal or call a method to trigger the repaint in the UI
+    emit repaintGameView();
 }
--- FastAsFox/FastAsFoxGame/game.h
+++ FastAsFox/FastAsFoxGame/game.h
@@ -10,34 +10,13 @@ class Game : public QObject
     Q_OBJECT
 
 public:
-    Game(QObject *parent = nullptr)
-        : QObject(parent),
-          tps(new QTimer(this)),
-          fps(new QTimer(this))
-    {
-        connect(tps, &QTimer::timeout, this, &Game::slotPositionUpdates);
-        tps->start(1000 / 20);  // 20 Hz
-
-        connect(fps, &QTimer::timeout, this, &Game::slotRepaintingUpdates);
-        fps->start(1000 / 100); // 100 Hz
-    }
-
-    ~Game()
-    {
-        delete tps;
-        delete fps;
-    }
+    Game(QObject *parent = nullptr);
+    ~Game();
 
 public slots:
-    void slotPositionUpdates()
-    {
-        // position update logic here
-    }
+    void slotPositionUpdates();
 
-    void slotRepaintingUpdates()
-    {
-        // repainting update logic here
-    }
+    void slotRepaintingUpdates();
 
 private:
     QTimer *tps;
