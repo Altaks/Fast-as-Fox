079916f89a991f086f7828cddf19953d8b8ab6ee
Arnaud Mazurier <amazur01@etudiant.univ-lr.fr>
2023-06-06 10:42:30 +0200

    Added a tileid into the tile class

--- FastAsFox/FastAsFoxGame/tile.h
+++ FastAsFox/FastAsFoxGame/tile.h
@@ -10,9 +10,11 @@ class Tile : public GameObject
 private:
     QPixmap * texture;
     QGraphicsPixmapItem * tile;
+    int tileid;
 public:
-    explicit Tile(QPixmap * texture, QObject *parent = nullptr);
+    explicit Tile(QPixmap * texture, int tileid, QObject *parent = nullptr);
     QGraphicsPixmapItem * getTileItem();
+    int getTileid() const;
 };
 
 #endif // TILE_H
