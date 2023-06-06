81c7baaf864d883098ff63534485dff9378f6cf3
Thomas Maynard <thomas.maynard@etudiant.univ-lr.fr>
2023-06-06 17:00:32 +0200

    Revert "correction des bugs trouvés"
    This reverts commit 0a41c430ccc4dd8d1d1d076b658c3784ceb80d1f

--- FastAsFox/FastAsFoxGame/level.cpp
+++ FastAsFox/FastAsFoxGame/level.cpp
@@ -1,6 +1,6 @@
 #include "level.h"
 
-Level::Level(pair<int,int> AStartingPosition, pair<int,int> AnEndingPosistion, Map * AMap)
+Level::Level(pair<int,int> AStartingPosition, pair<int,int> AnEndingPosistion,Map AMap)
 {
     startingPosition=AStartingPosition;
     endingPosition=AnEndingPosistion;
@@ -9,11 +9,11 @@ Level::Level(pair<int,int> AStartingPosition, pair<int,int> AnEndingPosistion, M
 
 Level::~Level(){
     player=nullptr;
-    map= nullptr;
 }
 
 
 void Level::loadMap(){
+    map.load();
 }
 
 void Level::showMap(){
@@ -37,7 +37,7 @@ void Level::finish(){
 
 }
 
-Map* Level::getMap(){
+Map Level::getMap(){
     return map;
 }
 
