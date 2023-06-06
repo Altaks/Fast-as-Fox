d1c2bb4bb3f68669fc476d4f5c557fa57efe7a1d
Hugo Russeil <hugo.russeil@etudiant.univ-lr.fr>
2023-06-06 17:41:37 +0200

    Documented pretty much everything

--- FastAsFox/FastAsFoxGame/animatedsprite.h
+++ FastAsFox/FastAsFoxGame/animatedsprite.h
@@ -9,37 +9,87 @@
 #include <QGraphicsPixmapItem>
 #include <QElapsedTimer>
 
+/**
+ * @brief The Fox class represents a fox character in a game.
+ *
+ * The Fox class inherits from QObject and QGraphicsPixmapItem and provides functionality
+ * for animating and controlling the fox's movements.
+ */
 class Fox : public QObject, public QGraphicsPixmapItem {
 
 public:
+    /**
+     * @brief Constructs a Fox object with the given parent QGraphicsScene.
+     * @param parentScene The parent QGraphicsScene in which the Fox will be placed.
+     */
     explicit Fox(QGraphicsScene *parentScene);
+
+    /**
+     * @brief Updates the current frame of the fox animation.
+     */
     void updateFrame();
+
+    /**
+     * @brief Starts the running animation of the fox.
+     */
     void startRunning();
+
+    /**
+     * @brief Stops the running animation of the fox.
+     */
     void stopRunning();
+
+    /**
+     * @brief Makes the fox jump.
+     */
     void jump();
+
+    /**
+     * @brief Makes the fox crouch.
+     */
     void crouch();
+
+    /**
+     * @brief Resets the fox's position and state to default.
+     */
     void reset();
+
+    /**
+     * @brief Returns the position of the fox sprite.
+     * @return The position of the fox sprite as a QPointF.
+     */
     QPointF getSpritePosition() const;
+
+    /**
+     * @brief Returns the pixmap of the current fox sprite.
+     * @return The pixmap of the current fox sprite as a QPixmap.
+     */
     QPixmap getSpritePixmap() const;
 
 private:
-    QPixmap *walkSpriteSheet;
-    QPixmap *runSpriteSheet;
-    QGraphicsScene *scene;
-    QTimer *timer;
-    QElapsedTimer *elapsedTimer;
-    int currentFrame;
-    bool isRunning;
-    QPointF spritePosition;
-    QPointF spriteVelocity;
-    const float groundLevel = 125.0f;
-    const QPointF gravity = QPointF(0, 2); // 1 or 2 or 3 ... > increasing gravity
+    QPixmap *walkSpriteSheet; /**< The walk animation sprite sheet for the fox. */
+    QPixmap *runSpriteSheet; /**< The run animation sprite sheet for the fox. */
+    QGraphicsScene *scene; /**< The parent QGraphicsScene of the fox. */
+    QTimer *timer; /**< The timer used for updating the fox animation. */
+    QElapsedTimer *elapsedTimer; /**< The elapsed timer used for frame rate control. */
+    int currentFrame; /**< The index of the current frame in the sprite sheet. */
+    bool isRunning; /**< A flag indicating whether the fox is currently running. */
+    QPointF spritePosition; /**< The position of the fox sprite. */
+    QPointF spriteVelocity; /**< The velocity of the fox sprite. */
+    const float groundLevel = 125.0f; /**< The y-coordinate of the ground level. */
+    const QPointF gravity = QPointF(0, 2); /**< The gravity applied to the fox. */
+
+    /**
+     * @brief Updates the pixmap of the fox sprite based on the current frame.
+     */
     void updatePixmap();
 
 signals:
+    /**
+     * @brief Signal emitted when the fox reaches the end of the map.
+     */
     void endOfMapReached();
 
 };
-
 
 #endif // ANIMATEDSPRITE_H
--- FastAsFox/FastAsFoxGame/mapsection.h
+++ FastAsFox/FastAsFoxGame/mapsection.h
@@ -4,15 +4,41 @@
 #include <iostream>
 #include <map>
 
+/**
+ * @brief The MapSection class represents a section of the game map.
+ *
+ * A MapSection is a portion of the game map that contains tiles.
+ */
 class MapSection
 {
 private:
-    std::map<std::pair<int, int>, int> coordinatesToTileId;
-    int sectionWidth, sectionHeight;
+    std::map<std::pair<int, int>, int> coordinatesToTileId; /**< The mapping of coordinates to tile IDs. */
+    int sectionWidth; /**< The width of the section. */
+    int sectionHeight; /**< The height of the section. */
+
 public:
+    /**
+     * @brief Constructs a MapSection object with the specified level path.
+     * @param levelpath The path to the level file.
+     */
     MapSection(std::string levelpath);
+
+    /**
+     * @brief Returns the mapping of coordinates to tile IDs.
+     * @return The mapping of coordinates to tile IDs.
+     */
     std::map<std::pair<int, int>, int> getCoordinatesToTileId() const;
+
+    /**
+     * @brief Returns the width of the section.
+     * @return The width of the section.
+     */
     int getSectionWidth() const;
+
+    /**
+     * @brief Returns the height of the section.
+     * @return The height of the section.
+     */
     int getSectionHeight() const;
 };
 
