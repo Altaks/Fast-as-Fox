c4d8a2398731792ed941f54ce2e58be9379d55d9
Hugo Russeil <hugo.russeil@etudiant.univ-lr.fr>
2023-06-09 09:37:34 +0200

    Created the documentation for the TileSet class

--- FastAsFox/FastAsFoxGame/tileset.h
+++ FastAsFox/FastAsFoxGame/tileset.h
@@ -8,15 +8,44 @@
 class TileSet
 {
 private:
+    /**
+     * @brief startingIndex the index at the beginning of the tileset
+     */
     int startingIndex;
+    /**
+     * @brief tiles map containing the tiles of the tileset, as well as their ID
+     */
     std::map<int, QPixmap*>* tiles = nullptr;
+    /**
+     * @brief split Splits the tileset to get each tile separately
+     * @return map containing the tiles of the tileset, as well as their ID
+     */
     std::map<int, QPixmap*>* split();
+    /**
+     * @brief filepath the path to the file containing the tileset
+     */
     std::string filepath;
+    /**
+     * @brief sideSize the size of a side of the tileMap
+     */
     int sideSize;
 public:
+    /**
+     * @brief TileSet the constructor for the TileSet class
+     * @param filepath the path to the file containing the tileset
+     * @param sideSize the size of a side of the tileMap
+     * @param startingIndex the index at the beginning of the tileset
+     */
     TileSet(std::string filepath, int sideSize, int startingIndex);
+    /**
+     * @brief TileSet the default constructor for the TileSet class
+     */
     TileSet();
     ~TileSet();
+    /**
+     * @brief load loads the tiles in the tileset
+     * @return a map containing the sprite of every tile, as well as their ID
+     */
     std::map<int, QPixmap*>* load();
 };
 
