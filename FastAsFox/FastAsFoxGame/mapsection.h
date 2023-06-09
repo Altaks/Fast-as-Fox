c02c4b76b7771693c2fdc492b2d34d1eb87f2904
Hugo Russeil <hugo.russeil@etudiant.univ-lr.fr>
2023-06-09 08:55:32 +0200

    Created the documentation for the mapSection class

--- FastAsFox/FastAsFoxGame/mapsection.h
+++ FastAsFox/FastAsFoxGame/mapsection.h
@@ -7,12 +7,35 @@
 class MapSection
 {
 private:
+    /**
+     * @brief coordinatesToTileId stores the coordinates of a tile, as well as its ID
+     */
     std::map<std::pair<int, int>, int> * coordinatesToTileId;
+    /**
+     * @brief sectionWidth the width of the mapSection
+     * @brief sectionHeight the height of the mapSection
+     */
     int sectionWidth, sectionHeight;
 public:
+    /**
+     * @brief MapSection the constructor for the mapSection class
+     * @param levelpath the path to the file that contains the level
+     */
     MapSection(std::string levelpath);
+    /**
+     * @brief getCoordinatesToTileId getter for coordinaeToTileId
+     * @return  coordinaeToTileId
+     */
     std::map<std::pair<int, int>, int> * getCoordinatesToTileId() const;
+    /**
+     * @brief getSectionWidth getter for sectionWidth
+     * @return sectionWidth
+     */
     int getSectionWidth() const;
+    /**
+     * @brief getSectionHeight getter for sectionHeight
+     * @return sectionHeight
+     */
     int getSectionHeight() const;
 };
 
