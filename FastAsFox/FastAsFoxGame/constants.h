58c0b702dc9fbd8d816520cccf10eac6dc3fa8bc
Altaks <57632793+Altaks@users.noreply.github.com>
2023-06-15 23:48:29 +0200

    Moved consts to constants.h

--- FastAsFox/FastAsFoxGame/constants.h
+++ FastAsFox/FastAsFoxGame/constants.h
@@ -73,6 +73,12 @@ const std::vector<std::pair<int,int>> END_BERRIES = {END_BERRIES_LEVEL1};
 
 
 // ----------------< POWER UPS CONSTANTS >------------------ //
+const static std::string DASH =          ":/powerups/sprites/powerups/apple.png" ;
+const static std::string DOUBLE_JUMP =   ":/powerups/sprites/powerups/cerise.png";
+const static std::string CHRONO_FREEZE = ":/powerups/sprites/powerups/raisin.png";
+const static std::string HEALTH =        ":/powerups/sprites/powerups/peche.png" ;
+const static std::string SPEED =         ":/powerups/sprites/powerups/citron.png";
+
 const double DASH_BOOST_PERCENTAGE = 170;
 const double DASH_DURATION = 2;
 
